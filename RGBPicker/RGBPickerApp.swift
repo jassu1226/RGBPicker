6c00bd3a8e90d7555c4f941c7aa7fa2896707805
jaswanthalaharigit <50083065+jassu1226@users.noreply.github.com>
2023-09-07 07:05:36 -0700

    Initial Commit

---
+++ RGBPicker/RGBPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBPickerApp.swift
+//  RGBPicker
+//
+//  Created by Rijutha Kona on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct RGBPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
